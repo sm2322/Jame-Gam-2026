b75afcb5ed4685d3e50d769811eac160fabc9280
Shepard M <smullins232@gmail.com>
2026-07-24 17:22:18 -0400

    Updates

--- dialouge scripts/Diolouge/texture_progress_bar.gd
+++ dialouge scripts/Diolouge/texture_progress_bar.gd
@@ -2,42 +2,88 @@ extends TextureProgressBar
 
 @onready var timer = $Timer
 @export var response_menu: Control
+@export var length = 3.0
 var first_run = true
-# Called when the node enters the scene tree for the first time.
+var _waiting_for_typing: bool = false
+var _last_dialogue_id: String = ""
+
 func _ready() -> void:
-	pass
+	var dialogue_label = _get_dialogue_label()
+	if is_instance_valid(dialogue_label):
+		dialogue_label.finished_typing.connect(_on_dialogue_finished_typing)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var dialogue_line: DialogueLine = null
+	var dialogue_line = _get_dialogue_line()
+	if dialogue_line == null:
+		return
+
+	if dialogue_line.id != _last_dialogue_id:
+		_reset_for_new_dialogue_line(dialogue_line.id)
+
+	if dialogue_line.responses.size() == 0:
+		if not timer.is_stopped():
+			timer.stop()
+		value = 100.0
+		return
+
+	var dialogue_label = _get_dialogue_label()
+	if timer.is_stopped() and first_run:
+		if is_instance_valid(dialogue_label) and dialogue_label.is_typing:
+			_waiting_for_typing = true
+			return
+
+		_waiting_for_typing = false
+		timer.start(length)
+		value = 100.0
+	elif not timer.is_stopped() and first_run:
+		value = timer.time_left * 100
+
+	if !first_run and Input.is_action_just_pressed("ui_accept"):
+		first_run = true
+
+
+func _on_dialogue_finished_typing() -> void:
+	_waiting_for_typing = false
+	if first_run and timer.is_stopped():
+		timer.start(length)
+		value = 100.0
+
+
+func _reset_for_new_dialogue_line(dialogue_id: String) -> void:
+	_last_dialogue_id = dialogue_id
+	first_run = true
+	_waiting_for_typing = false
+	if not timer.is_stopped():
+		timer.stop()
+	value = 100.0
+
+
+func _get_dialogue_line():
 	var parent_node = get_parent()
 	if is_instance_valid(parent_node):
-		dialogue_line = parent_node.get("dialogue_line")
-	if dialogue_line == null:
-		dialogue_line = await DialogueManager.get_next_dialogue_line(Globals.CurrentScene, "start")
-	print(first_run)
-	print(dialogue_line)
-	if dialogue_line.responses.size() > 0:
-		if timer.is_stopped() and first_run:
-			timer.start(3)
-		elif first_run:
-			$".".value = timer.time_left * 100
-	if !first_run:
-		if Input.is_action_just_pressed("ui_accept"):
-			first_run = true	
-			print("First run on")
-	
+		var dialogue_line = parent_node.get("dialogue_line")
+		if dialogue_line != null:
+			return dialogue_line
+	return null
+
+
+func _get_dialogue_label():
+	var parent_node = get_parent()
+	if is_instance_valid(parent_node):
+		var dialogue_label = parent_node.get("dialogue_label")
+		if is_instance_valid(dialogue_label):
+			return dialogue_label
+	return null
+
+
 func _on_timer_timeout() -> void:
-	var dialogue_line: DialogueLine = null
-	var parent_node = get_parent()
-	if is_instance_valid(parent_node):
-		dialogue_line = parent_node.get("dialogue_line")
+	var dialogue_line = _get_dialogue_line()
 	if dialogue_line == null:
-		dialogue_line = await DialogueManager.get_next_dialogue_line(Globals.CurrentScene, "start")
+		return
 	if dialogue_line.responses.size() > 0 and first_run:
 		var random_index: int = randi() % dialogue_line.responses.size()
 		var random_option = dialogue_line.responses[random_index]
 		get_parent().goNext(random_option)
 		first_run = false
-		print("First run off")
+		value = 0.0
