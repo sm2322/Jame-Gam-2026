92492807e066cd7d4b72118cba9103b057866f46
Demetria White <luvdemixo@hotmail.com>
2026-07-24 18:41:24 -0400

    Add files via upload

---
+++ node_2d.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var aura_frame := 0
+var max_frame := 11
+
+@onready var aura_bar = $AuraBar
+
+func _on_increase_aura_button_pressed():
+	print("aura increased!")
+	aura_frame += 1
+	aura_frame = clamp(aura_frame, 0, max_frame)
+	aura_bar.frame = aura_frame
+
+
+func _on_decrease_aura_button_pressed():
+	print("aura decreased!")
+	aura_frame -= 2
+	aura_frame = clamp(aura_frame, 0, max_frame)
+	aura_bar.frame = aura_frame
